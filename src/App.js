f465d9188ec65047223041a260f665a066fd74f3
hothitiep02 <htt301202@gmail.com>
2025-05-05 22:12:45 +0700

    SCRUM-15 | Create student accounts form - Tiep

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import CreateStudentForm from './pages/admin/CreateStudentForm';
 
 function App() {
   return (
-    <></>
+    <CreateStudentForm></CreateStudentForm>
   );
 }
 
