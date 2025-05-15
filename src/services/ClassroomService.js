34763c85a8677ccb480c84e2a4f64d8c79bde855
LeKyBa <lekyba2000hc@gmail.com>
2025-05-15 14:26:26 +0700

    SCRUM-70 | fix BR_005 - show the classrooms when create student accounts - kyba

--- src/services/ClassroomService.js
+++ src/services/ClassroomService.js
@@ -2,8 +2,8 @@ import apiClient from "./apiClient";
 
 export async function getAllClassrooms() {
     try {
-      const response = await apiClient.get("/api/user/teacher/classrooms");
-      return response.data;
+      const response = await apiClient.get("/api/classrooms");
+      return response;
     } catch (error) {
       console.error("Error while fetching classrooms:", error);
       return [];
