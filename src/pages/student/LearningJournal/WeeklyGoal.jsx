0ab266c0d1d70b3f8c8ec23bb3e95a3193a2f3cf
MaiTram01 <tramh7879@gmail.com>
2025-05-17 04:13:49 +0700

    SCRUM-32 | create static UI for learning journal - maitram

---
+++ src/pages/student/LearningJournal/WeeklyGoal.jsx
@@ -0,0 +1,19 @@
+export function WeeklyGoal({goals, goalIndex}) {
+    return (
+        <div className="student-selfstudy-goals" style={{ flex: 1 }}>
+                {goals.slice(goalIndex, goalIndex + 3).map((goal, index) => (
+                  <div
+                    key={goalIndex + index}
+                    className="student-selfstudy-goal"
+                  >
+                    <input
+                      type="checkbox"
+                      name={`goal_${goalIndex + index}`}
+                      id={`goal_${goalIndex + index}`}
+                    />
+                    <label htmlFor={`goal_${goalIndex + index}`}>{goal}</label>
+                  </div>
+                ))}
+              </div>
+    )
+}
--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -2,9 +2,12 @@ import StudentLayout from "../components/layouts/MainLayouts/StudentLayout";
 import Home from '../pages/student/Home/Home';
 import { Route } from "react-router-dom";
 import { ProtectedRoute } from "./ProtectedRoutes";
+import { SelfStudy } from "../pages/student/LearningJournal/SelfStudy";
+import { LearningJournalLayout } from "../pages/student/LearningJournal/LearningJournalLayout";
 
 const studentRoutes = [
     {path: '/student/home', element: <Home/> },
+    {path: '/student/learning-journal', element: <LearningJournalLayout><SelfStudy/></LearningJournalLayout> },
 ]
 export function StudentRoutes() {
     return studentRoutes.map(({ path, element }) => (
