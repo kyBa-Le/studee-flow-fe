e99b5587105f38024b1b2848c1b49eee2bade056
kimsa-0605 <holykimsa05@gmail.com>
2025-05-24 10:50:21 +0700

    SCRUM-18 | build create classroom form - kimsa

--- src/pages/admin/ClassroomManagement/ClassroomManagement.jsx
+++ src/pages/admin/ClassroomManagement/ClassroomManagement.jsx
@@ -1,52 +1,58 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./ClassroomManagement.css";
 import { ClassroomList } from "./ClassroomList";
 import { ClassroomInfo } from "./ClassroomInfo";
 import { adminGetAllClassrooms } from "../../../services/ClassroomService";
+import { CreateClassroomForm } from "./CreateClassroomForm";
 
 export function ClassroomManagement() {
     const [classrooms, setClassrooms] = useState([]);
     const [selectedClassroom, setSelectedClassroom] = useState(0);
+    const [isShowCreateForm, setIsShowCreateForm] = useState(false);
 
     useEffect(() => {
         const fetchClassrooms = async () => {
-            console.log("run");
             try {
                 const fetchedClassrooms = await adminGetAllClassrooms();
-                console.log("Fetched classrooms:", fetchedClassrooms);
                 setClassrooms(fetchedClassrooms.data);
             } catch (error) {
                 console.error("Error fetching classrooms:", error);
             }
-
-        }
+        };
 
         fetchClassrooms();
     }, []);
 
-    useEffect(
-        () => {
-            console.log(selectedClassroom);
-        },
-        [selectedClassroom]
-    )
+    const handleShowCreateForm = () => {
+        setIsShowCreateForm(!isShowCreateForm);
+    };
 
-return (
-    <div className="classroom-management-container">
-        <div className="add-and-search-container">
-            <button id="add-button">+ Create class</button>
-            <form id="search-box">
-                <input type="text" placeholder="Search ..." />
-                <div id="search-icon">
-                    <i className="fa-solid fa-magnifying-glass"></i>
-                </div>
-            </form>
-        </div>
+    const handleCreate = (formData) => {
+        // PostData
+    }
 
-        <div className="classroom-main-content-container">
-            <ClassroomList setSelectedClassroom={setSelectedClassroom} classrooms={classrooms} />
-            <ClassroomInfo classroom={classrooms[selectedClassroom]} />
+    return (
+        <div className="classroom-management-container">
+            <div className="add-and-search-container">
+                <button id="add-button" onClick={handleShowCreateForm}>
+                    + Create class
+                </button>
+                <form id="search-box">
+                    <input type="text" placeholder="Search ..." />
+                    <div id="search-icon">
+                        <i className="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                </form>
+            </div>
+
+            <div className="classroom-main-content-container">
+                <ClassroomList
+                    setSelectedClassroom={setSelectedClassroom}
+                    classrooms={classrooms}
+                />
+                <ClassroomInfo classroom={classrooms[selectedClassroom]} />
+            </div>
+            {isShowCreateForm && <CreateClassroomForm handleShowCreateForm={handleShowCreateForm} handleCreate={handleCreate} />}
         </div>
-    </div>
-);
+    );
 }
